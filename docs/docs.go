a56304c256a7728ca2dbffa2ea36c5a0029d5128
Sayem Chowdhury <sayem314@gmail.com>
2024-10-10 21:27:56 +0600

    update port

--- docs/docs.go
+++ docs/docs.go
@@ -73,7 +73,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "127.0.0.1:8484",
+	Host:             "127.0.0.1:3031",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "PhoenixUptime API",
