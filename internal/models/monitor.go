814c9fd6413e6b876cbf4f60d9438a0a83767326
Sayem Chowdhury <sayem314@gmail.com>
2024-10-10 21:27:40 +0600

    update monitor

--- internal/models/monitor.go
+++ internal/models/monitor.go
@@ -6,16 +6,16 @@ type Monitor struct {
 	UserID     uint             `json:"user_id" gorm:"index:idx_user_id"`
 	Name       string           `gorm:"not null" json:"name" swagger:"example=My URL Monitor"`
 	URL        string           `gorm:"not null" json:"url" swagger:"example=https://example.com"`
-	Interval   int              `gorm:"default:60" json:"interval" swagger:"example=60"`
-	Status     Status           `gorm:"default:'unknown'" json:"status" swagger:"example=up"`
+	Interval   int              `gorm:"default:60" json:"interval" swagger:"example=60"` // Interval in seconds
+	Status     Status           `gorm:"type:enum('up','down','paused','unknown');default:'unknown'" json:"status" swagger:"example=up"`
 	Type       MonitorType      `gorm:"not null" json:"type" swagger:"example=url"`
-	Method     *string          `json:"method,omitempty" swagger:"example=GET"`
-	Filters    Filters          `gorm:"embedded" json:"filters"`
+	Method     *string          `json:"method,omitempty" swagger:"example=GET"`                    // For URL monitors
+	Filters    *Filters         `gorm:"embedded;embeddedPrefix:filters_" json:"filters,omitempty"` // Optional filters
 	Tags       []Tag            `gorm:"many2many:monitor_tags;joinForeignKey:MonitorID;joinReferences:TagID" json:"tags"`
-	SMTPConfig SMTPMonitor      `gorm:"embedded" json:"smtp_config,omitempty"`
+	SMTPConfig *SMTPMonitor     `gorm:"embedded;embeddedPrefix:smtp_" json:"smtp_config,omitempty"` // Optional SMTP settings
 	History    []MonitorHistory `gorm:"foreignKey:MonitorID" json:"history"`
-	Retry      int              `json:"retry" gorm:"default:3" swagger:"example=3"`
-	RetryAfter int              `json:"retry_after" gorm:"default:30" swagger:"example=30"`
+	Retry      int              `json:"retry" gorm:"default:3" swagger:"example=3"`         // Number of retries
+	RetryAfter int              `json:"retry_after" gorm:"default:30" swagger:"example=30"` // Seconds between retries
 }
 
 // MonitorHistory struct to hold history related to each monitor
